410bbd8b273713e184cbed32c70e371e5e9e1fac
luizavila88 <luiz.nascimento.avila@gmail.com>
2026-03-02 16:43:46 -0300

    Código atividade
    Código com implementação dos algoritmos de ordenação

---
+++ Atividade02-Semana03/sorting_algorithms.c
@@ -0,0 +1,221 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX_SIZE 10000
+
+void printArray(int array[], int n) {
+	int i;
+	for (i = 0; i < n; i++) {
+		printf("%d ", array[i]);	
+	}
+	printf("\n");
+}
+ 
+void bubbleSort(int arr[], int n) {
+	int i, j, temp;
+	for (i = 0; i < n-1; i++){
+		for (j = 0; j < n-i-1; j++){
+			if (arr[j] > arr[j+1]){
+				temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp;
+			}
+		}
+	}
+}
+
+void selectionSort(int arr[], int n) {
+	int i, j, min_idx, temp;
+	for (i = 0; i < n-1; i++){
+		min_idx = i;
+		for (j = i+1; j < n; j++){
+			if (arr[j] < arr[min_idx]) {
+				min_idx = j;
+			}
+		}
+		temp = arr[min_idx];
+		arr[min_idx] = arr[i];
+		arr[i] = temp;
+	}
+}
+
+void insertionSort(int arr[], int n) {
+	int i, key, j;
+	for (i = 1; i < n; i++){
+		key = arr[i];
+		j = i - 1;
+		while (j >= 0 && arr[j] > key) {
+			arr[j + 1] = arr[j];
+			j = j - 1;
+		}
+		arr[j + 1] = key;
+	}
+	
+}
+
+int partition(int arr[], int low, int high) {
+	int pivot = arr[high];
+	int i = low - 1;
+	int j;
+	for (j = low; j < high; j++) {
+		if (arr[j] < pivot) {
+			i++;
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+		}
+	}
+	
+	int temp = arr[i + 1];
+	arr[i + 1] = arr[high];
+	arr[high] = temp;
+	
+	return ( i + 1);
+}
+
+void quickSort(int arr[], int low, int high) {
+	if (low < high) {
+		int pi = partition(arr, low, high);
+		quickSort(arr, low, pi-1);
+		quickSort(arr, pi + 1, high);
+	}
+}
+
+void merge(int arr[], int l, int m, int r) {
+	int n1 = m - l + 1;
+	int n2 = r - m;
+	
+	int L[n1], R[n2];
+	
+	for (int i = 0; i < n1; i++)
+		L[i] = arr[l + i];
+	for (int j = 0; j < n2; j++)
+		R[j] = arr[m + 1 + j];
+		
+	int i = 0, j = 0, k = l;
+	
+	while (i < n1 && j < n2){
+		if (L[i] <= R[j]) {
+			arr[k] = L[i];
+			i++;
+		} else {
+			arr[k] = R[j];
+			j++;
+		}
+		k++;
+	}
+	
+	while (i < n1) {
+		arr[k] = L[i];
+		i++;
+		k++;
+	}
+	
+	while (j < n2) {
+		arr[k] = R[j];
+		j++;
+		k++;
+	}
+	
+}
+
+void mergeSort(int arr[], int l, int r) {
+	if ( l < r) {
+		int m = l + (r - l) / 2;
+		
+		mergeSort(arr, l, m);
+		mergeSort(arr, m + 1, r);
+		merge(arr, l, m, r);
+	}
+}
+
+void executarTeste(int opcao, int arr[], int size) {
+	int arrCopia[MAX_SIZE];
+	int i;
+	
+	for (i = 0; i < size; i++)
+		arrCopia[i] = arr[i];
+		
+	printf("Os 10 primeiros antes da ordenação:\n");
+	printArray(arrCopia, 10);
+	
+	clock_t inicio = clock();
+	
+	switch(opcao) {
+		case 1: 
+			bubbleSort(arrCopia, size); 
+			break;
+		case 2:
+			selectionSort(arrCopia, size);
+			break;
+		case 3:
+			insertionSort(arrCopia, size);
+			break;
+		case 4:
+			quickSort(arrCopia, 0, size - 1);
+			break;
+		case 5:
+			mergeSort(arrCopia, 0, size - 1);
+			break;
+	}
+	
+	clock_t fim = clock();
+	double tempo = ((double)(fim - inicio)) / CLOCKS_PER_SEC;
+	
+	printf("Depois da ordenação:\n");
+	printArray(arrCopia, 10);
+	
+	printf("Tempo com %d elementos: %f segundos\n", size, tempo);
+}
+
+int main() {
+	int arr[MAX_SIZE];
+	int i, opcao, tamanho;
+	
+	srand(time(NULL));
+	
+	for(i = 0; i < MAX_SIZE; i++) 
+		arr[i] = rand() % 1000;
+	
+	do {
+		printf("Escoha o algoritimo a ser executado:\n");
+		printf("1. bubbleSort\n");
+		printf("2. selectionSort\n");
+		printf("3. insertionSort\n");
+		printf("4. quickSort\n");
+		printf("5. mergeSort\n");
+		printf("0. Sair\n");
+		scanf("%d", &opcao);
+		
+		if (opcao == 0)
+		break;
+
+	printf("Escolha o tamanho do array:\n");
+	printf("1. 100\n");
+	printf("2. 1000\n");
+	printf("3. 10000\n");
+	int opcaoTamanho;
+	scanf("%d", &opcaoTamanho);
+	
+	switch (opcaoTamanho) {
+		case 1:
+			tamanho = 100;
+			break;
+		case 2: 
+			tamanho = 1000;
+			break;
+		case 3:
+			tamanho = 10000;
+			break;			
+	}
+
+	executarTeste(opcao, arr, tamanho);
+
+ } while (opcao != 0);
+ 
+ return 0;
+
+}
+
+   
